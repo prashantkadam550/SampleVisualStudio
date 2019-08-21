ba7f0373867f6ef433583fdfbee0455b7a609bdb
Prashant Kadam <prashantkadam550@gmail.com>
2019-08-21 16:44:33 +0530

    Initial Commit

---
+++ samplevisualstudio/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace samplevisualstudio
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String s1 = "Prashant Kadam";
+            MessageBox.Show("Hello World" + s1);
+            // This is the comment
+        }
+    }
+}
